190211194bfd754d5111fb636443641bdd010867
sexroute <zhaodali78@gmail.com>
2014-09-02 10:34:07 +0800

    [ADD] 增加FFTC++ 实现

---
+++ fftw/fftw/fftTest.cpp
@@ -0,0 +1,140 @@
+#include "stdafx.h"
+#include "fftwx.h"
+#include <math.h>
+#include <vector>
+#pragma comment(lib,"libfftw3-3.lib")
+#define _FFT_PI 3.1415926535898
+int CFFT_Wrapper::FFT(  double * apInput, 
+						double * apOutPutReal, 
+						double * apOutPutImg, 
+						int anInputLength, 
+						int& anOutputLength )
+{
+	//1. parameter check
+	if (NULL == apInput)
+	{
+		return ERR_NULL_INPUT_BUFFER;
+	}
+
+	if (NULL == apOutPutReal)
+	{
+		return ERR_NULL_OUTPUT_REAL_BUFFER;
+	}
+
+	if (NULL == apOutPutImg)
+	{
+		return ERR_NULL_OUTPUT_IMG_BUFFER;
+	}
+
+	if (anInputLength<=16)
+	{
+		return ERR_INVALID_INPUT_LENGTH;
+	}
+
+	if (anOutputLength<(anInputLength/2))
+	{
+		anOutputLength= ceil(double(anInputLength)/2);
+		return ERR_NOT_ENOUGH_OUTPUT_BUFFER_LENGTH;
+	}
+
+	//2.do fftw3
+	fftw_complex * lpOut = NULL;
+
+	fftw_plan p = NULL;
+	
+	lpOut = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * anInputLength);
+
+	p = fftw_plan_dft_r2c_1d(anInputLength,apInput,lpOut,FFTW_MEASURE);
+
+	fftw_execute(p);
+
+	fftw_destroy_plan(p);
+
+	for (int i=0;i<anOutputLength;i++)
+	{
+		apOutPutReal[i] = lpOut[i][0];
+		apOutPutImg[i]  = lpOut[i][1];
+	}
+
+	fftw_free(lpOut); 
+
+	return ERR_NO_ERROR;
+
+}
+
+int CFFT_Wrapper::FFT2( double * apInput, 
+					   double * apOutPutAmp, 
+					   double * apOutPutPhase, 
+					   int anInputLength, 
+					   int& anOutputLength )
+{
+	//1. parameter check
+	if (NULL == apInput)
+	{
+		return ERR_NULL_INPUT_BUFFER;
+	}
+
+	if (NULL == apOutPutAmp)
+	{
+		return ERR_NULL_OUTPUT_AMP_BUFFER;
+	}
+
+	if (NULL == apOutPutPhase)
+	{
+		return ERR_NULL_OUTPUT_PHASE_BUFFER;
+	}
+
+	if (anInputLength<=16)
+	{
+		return ERR_INVALID_INPUT_LENGTH;
+	}
+
+	if (anOutputLength<(anInputLength/2))
+	{
+		anOutputLength= ceil(double(anInputLength)/2);
+		return ERR_NOT_ENOUGH_OUTPUT_BUFFER_LENGTH;
+	}
+
+	//2.do fftw3
+	fftw_complex * lpOut = NULL;
+
+	std::vector<double> loInput;
+
+	loInput.resize(anInputLength);
+
+	double * lpInput = &loInput.front();
+
+	fftw_plan p = NULL;
+
+	lpOut = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * anInputLength);
+
+	p = fftw_plan_dft_r2c_1d(anInputLength,lpInput,lpOut,FFTW_MEASURE);
+
+	memcpy(lpInput,apInput,sizeof(double)*anInputLength);
+
+	fftw_execute_dft_r2c(p,apInput,lpOut);
+
+
+	for (int i=0;i<anOutputLength;i++)
+	{
+		double ldblReal = lpOut[i][0];
+		double ldblImage  = lpOut[i][1];
+		double ldblMag = sqrt((ldblReal * ldblReal) + (ldblImage * ldblImage))*4/anInputLength;
+		double ldblPhase = atan(ldblImage/ldblReal)/_FFT_PI*180.0;
+		ldblPhase = (int)(ldblPhase*100)%36000;
+		ldblPhase = ldblPhase/100;		
+		apOutPutAmp[i] = ldblMag;
+		apOutPutPhase[i]= ldblPhase;
+	}
+	
+	//´¦ÀíÁãµã
+	apOutPutAmp[0] = apOutPutAmp[0]/anInputLength;
+	apOutPutPhase[0] = apOutPutPhase[0]/anInputLength;
+
+	fftw_destroy_plan(p);
+
+	fftw_free(lpOut); 
+
+	return ERR_NO_ERROR;
+}
+
---
+++ fftw/fftw/fftwx.h
@@ -0,0 +1,75 @@
+#pragma once
+#include "fftw3.h"
+
+class CFFT_Wrapper
+{
+public:
+	//************************************
+	// Method:    FFT 返回实部虚部
+	// FullName:  CFFT_Wrapper::FFT
+	// Access:    public static 
+	// Returns:   int 返回值，参照_EER_CODE_
+	// Qualifier:
+	// Parameter: double * apInput		 输入实数一维数组 
+	// Parameter: double * apOutPutReal  输出实部数组
+	// Parameter: double * apOutPutImg   输出虚部数组 
+	// Parameter: int anInputLength		 输入长度
+	// Parameter: int & anOutputLength   输出长度，引用
+	//************************************
+	static int FFT( double * apInput,
+					double * apOutPutReal,
+					double * apOutPutImg,
+					int   anInputLength,
+					int& anOutputLength); 
+	
+	//************************************
+	// Method:    FFT2 返回幅值相位
+	// FullName:  CFFT_Wrapper::FFT2
+	// Access:    public static 
+	// Returns:   int 返回值，参照_EER_CODE_
+	// Qualifier:
+	// Parameter: double * apInput		 输入实数一维数组 
+	// Parameter: double * apOutPutAmp   输出幅值数组
+	// Parameter: double * apOutPutPhase 输出相位数组 
+	// Parameter: int anInputLength		 输入长度
+	// Parameter: int & anOutputLength   输出长度，引用
+	//************************************
+	static int FFT2( double * apInput,
+					double * apOutPutAmp,
+					double * apOutPutPhase,
+					int   anInputLength,
+					int& anOutputLength); 
+
+	//************************************
+	// Method:    FFT3 高性能FFT,采样点截止到2的整数次幂
+	// FullName:  CFFT_Wrapper::FFT3
+	// Access:    public static 
+	// Returns:   int
+	// Qualifier:
+	// Parameter: double * apInput
+	// Parameter: double * apOutPutAmp
+	// Parameter: double * apOutPutPhase
+	// Parameter: int anInputLength
+	// Parameter: int & anOutputLength
+	//************************************
+	static int FFT3( double * apInput,
+					 double * apOutPutAmp,
+					 double * apOutPutPhase,
+					 int   anInputLength,
+					 int& anOutputLength); 
+	enum _EER_CODE_
+	{
+		ERR_NO_ERROR = 0,
+		ERR_NULL_INPUT_BUFFER ,
+		ERR_NULL_OUTPUT_IMG_BUFFER ,
+		ERR_NULL_OUTPUT_REAL_BUFFER ,
+		ERR_INVALID_INPUT_LENGTH,
+		ERR_NOT_ENOUGH_OUTPUT_BUFFER_LENGTH,
+		ERR_NULL_OUTPUT_AMP_BUFFER ,
+		ERR_NULL_OUTPUT_PHASE_BUFFER ,
+	};
+protected:
+private:
+};
+
+
